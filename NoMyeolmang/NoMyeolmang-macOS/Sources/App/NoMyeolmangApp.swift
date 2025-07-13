a7eb2b129ea7cf1692d90e66a56b8982f7067235
Moo <mutopia82@gmail.com>
2025-07-13 12:37:17 +0900

    setting: #14 macOS 초기세팅
    - 타겟 설정 및 정상 빌드 확인

---
+++ NoMyeolmang/NoMyeolmang-macOS/Sources/App/NoMyeolmangApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoMyeolmang_macOSApp.swift
+//  NoMyeolmang-macOS
+//
+//  Created by Moo on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct NoMyeolmangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
