729ec820451fca410df70dd5270eb4d8cff82a2a
Moo <mutopia82@gmail.com>
2025-07-17 16:14:58 +0900

    feat: #23 minMaxScaler 추가

---
+++ NoMyeolmang/NoMyeolmang/Sources/ML/Utilities/MinMaxScaler.swift
@@ -0,0 +1,28 @@
+//
+//  MinMaxScaler.swift
+//  NoMyeolmang
+//
+//  Created by Moo on 7/15/25.
+//
+
+enum MinMaxScaler {
+    static func scale(value: Double, min: Double, max: Double) -> Double {
+        guard max != min else { return 0.0 }
+        return (value - min) / (max - min)
+    }
+    static func scaleBlink(_ value: Double) -> Double {
+        return scale(value: value, min: Constants.blinkCountPerMinValue, max: Constants.blinkCounterPerMaxValue)
+    }
+    
+    static func scaleFace(_ value: Double) -> Double {
+        return scale(value: value, min: Constants.faceBodyPresentMinValue, max: Constants.faceBodyPresentMaxValue)
+    }
+    
+    static func scalePhone(_ value: Double) -> Double {
+        return scale(value: value, min: Constants.phonePresentMinValue, max: Constants.phonePresentMaxValue)
+    }
+    
+    static func scaleTime(_ value: Double) -> Double {
+        return scale(value: value, min: Constants.elapsedTimeMinValue, max: Constants.elapsedTimeMaxValue)
+    }
+}
