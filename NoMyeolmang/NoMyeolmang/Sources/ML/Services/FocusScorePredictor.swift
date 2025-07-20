c95b4327298ec3cb549aa0691b5abc2e16cefadb
Moo <mutopia82@gmail.com>
2025-07-20 19:10:15 +0900

    feat: #23 예측 모듈 편의 생성자 추가 및 코드 정리
    - FocusScorePredictor가 커스텀 URL 또는 번들 모델로 초기화될 수 있도록 개선하였습니다. + 코드정리

--- NoMyeolmang/NoMyeolmang/Sources/ML/Services/FocusScorePredictor.swift
+++ NoMyeolmang/NoMyeolmang/Sources/ML/Services/FocusScorePredictor.swift
@@ -2,30 +2,31 @@
 //  FocusScorePredictor.swift
 //  NoMyeolmang
 //
-//  Created by Moo on 7/15/25.
+//  Updated by Moo on 7/20/25.
 //
 
 import CoreML
 
 final class FocusScorePredictor {
-    private let model: FocusScore_Updatable
+    private let model: MLModel
 
-    init?() {
-        guard let model = try? FocusScore_Updatable(configuration: MLModelConfiguration()) else { return nil }
+    init?(modelURL: URL) {
+        guard let model = try? MLModel(contentsOf: modelURL) else { return nil }
         self.model = model
-        
-        print(model.model.modelDescription.isUpdatable)
     }
-    
+
+    convenience init?() {
+        guard let bundleURL = Bundle.main.url(forResource: "FocusScore_Updatable", withExtension: "mlmodelc") else { return nil }
+        self.init(modelURL: bundleURL)
+    }
+
     func run(input: MLModelInput) -> Double? {
         guard let inputArray = makeModelInputArray(input: input) else { return nil }
         return predict(inputArray: inputArray)
     }
-    
+
     private func makeModelInputArray(input: MLModelInput) -> MLMultiArray? {
-        guard let inputArray = try? MLMultiArray(shape: [1, 4], dataType: .float32) else {
-            return nil
-        }
+        guard let inputArray = try? MLMultiArray(shape: [1, 4], dataType: .float32) else { return nil }
         
         inputArray[0] = NSNumber(value: MinMaxScaler.scaleBlink(input.blinkCountPerMin))
         inputArray[1] = NSNumber(value: MinMaxScaler.scaleFace(input.faceBodyPresent))
@@ -35,7 +36,9 @@ final class FocusScorePredictor {
     }
 
     private func predict(inputArray: MLMultiArray) -> Double? {
-        guard let result = try? model.prediction(dense_1_input: inputArray) else { return nil }
-        return result.Identity[0].doubleValue
+        guard let inputFeatures = try? MLDictionaryFeatureProvider(dictionary: ["dense_1_input": inputArray]),
+              let result = try? model.prediction(from: inputFeatures),
+              let outputArray = result.featureValue(for: "Identity")?.multiArrayValue else { return nil }
+        return outputArray[0].doubleValue
     }
 }
