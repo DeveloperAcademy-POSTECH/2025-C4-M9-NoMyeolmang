1f3ceaeebcefd79de538154b412ce3f8cff1535b
wish627 <wishful.to@gmail.com>
2025-07-21 11:52:16 +0900

    feat: #41 전체 폰트 등록
    Pretendard, ExtraBold, 20
    Pretendard, SemiBold, 28
    Pretendard, SemiBold, 24
    Pretendard, SemiBold, 20
    Pretendard, SemiBold, 18
    Pretendard, SemiBold, 14
    Pretendard, SemiBold, 16
    Pretendard, Regular, 16
    Pretendard, Regular, 14
    Pretendard, Light, 16
    Pretendard, Light, 14
    Pretendard, Light, 12
    Spoqa Han Sans Neo, Bold, 52

---
+++ NoMyeolmang/NoMyeolmang/Sources/DesignSystem/GSFont.swift
@@ -0,0 +1,35 @@
+//
+//  GSFont.swift
+//  NoMyeolmang
+//
+//  Created by 김소원 on 7/21/25.
+//
+
+import SwiftUI
+
+enum GSFont {
+    
+    // Pretendard Bold
+    static let pretendardBold36: Font = .custom("Pretendard-Bold", size: 36)
+    static let pretendardBold32: Font = .custom("Pretendard-Bold", size: 32)
+    static let pretendardBold28: Font = .custom("Pretendard-Bold", size: 28)
+    static let pretendardBold24: Font = .custom("Pretendard-Bold", size: 24)
+    static let pretendardBold16: Font = .custom("Pretendard-Bold", size: 16)
+    static let pretendardBold14: Font = .custom("Pretendard-Bold", size: 14)
+
+    // Pretendard Medium
+    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 16)
+    static let pretendardMedium14: Font = .custom("Pretendard-Medium", size: 14)
+
+    // Pretendard Regular
+    static let pretendardRegular16: Font = .custom("Pretendard-Regular", size: 16)
+    static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
+
+    // Pretendard Light
+    static let pretendardLight16: Font = .custom("Pretendard-Light", size: 16)
+    static let pretendardLight14: Font = .custom("Pretendard-Light", size: 14)
+    static let pretendardLight12: Font = .custom("Pretendard-Light", size: 12)
+
+    // Spoqa Han Sans Neo
+    static let spoqaBold52: Font = .custom("SpoqaHanSansNeo-Bold", size: 52)
+}
