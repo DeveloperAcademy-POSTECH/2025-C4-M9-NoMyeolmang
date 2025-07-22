9e2fb856a042341030f8b1f1e5f5f20e4792b772
wish627 <wishful.to@gmail.com>
2025-07-22 14:34:19 +0900

    refactor: #44 자간 행간 반영 코드 리팩토링
    자간, 행간 반영 시 폰트 크기를 지정하지 않아도 되게 바꿈 !

--- NoMyeolmang/NoMyeolmang/Sources/DesignSystem/GSFont.swift
+++ NoMyeolmang/NoMyeolmang/Sources/DesignSystem/GSFont.swift
@@ -7,29 +7,34 @@
 
 import SwiftUI
 
+struct GSFontStyle {
+    let font: Font
+    let size: CGFloat
+}
+
 enum GSFont {
     
     // Pretendard-ExtraBold
-    static let ExtraBold20: Font = .custom("Pretendard-ExtraBold", size: 20)
+    static let ExtraBold20 = GSFontStyle(font: .custom("Pretendard-ExtraBold", size: 20), size: 20)
     
     // Pretendard-SemiBold
-    static let SemiBold28: Font = .custom("Pretendard-SemiBold", size: 28)
-    static let SemiBold24: Font = .custom("Pretendard-SemiBold", size: 24)
-    static let SemiBold20: Font = .custom("Pretendard-SemiBold", size: 20)
-    static let SemiBold18: Font = .custom("Pretendard-SemiBold", size: 18)
-    static let SemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
-    static let SemiBold14: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let SemiBold28 = GSFontStyle(font: .custom("Pretendard-SemiBold", size: 28), size: 28)
+    static let SemiBold24 = GSFontStyle(font: .custom("Pretendard-SemiBold", size: 24), size: 24)
+    static let SemiBold20 = GSFontStyle(font: .custom("Pretendard-SemiBold", size: 20), size: 20)
+    static let SemiBold18 = GSFontStyle(font: .custom("Pretendard-SemiBold", size: 18), size: 18)
+    static let SemiBold16 = GSFontStyle(font: .custom("Pretendard-SemiBold", size: 16), size: 16)
+    static let SemiBold14 = GSFontStyle(font: .custom("Pretendard-SemiBold", size: 14), size: 14)
 
     // Pretendard Regular
-    static let Regular16: Font = .custom("Pretendard-Regular", size: 16)
-    static let Regular14: Font = .custom("Pretendard-Regular", size: 14)
-    static let Regular12: Font = .custom("Pretendard-Regular", size: 12)
+    static let Regular16 = GSFontStyle(font: .custom("Pretendard-Regular", size: 16), size: 16)
+    static let Regular14 = GSFontStyle(font: .custom("Pretendard-Regular", size: 14), size: 14)
+    static let Regular12 = GSFontStyle(font: .custom("Pretendard-Regular", size: 12), size: 12)
 
     // Pretendard Light
-    static let Light16: Font = .custom("Pretendard-Light", size: 16)
-    static let Light14: Font = .custom("Pretendard-Light", size: 14)
-    static let Light12: Font = .custom("Pretendard-Light", size: 12)
+    static let Light16 = GSFontStyle(font: .custom("Pretendard-Light", size: 16), size: 16)
+    static let Light14 = GSFontStyle(font: .custom("Pretendard-Light", size: 14), size: 14)
+    static let Light12 = GSFontStyle(font: .custom("Pretendard-Light", size: 12), size: 12)
 
     // Spoqa Han Sans Neo
-    static let spoqaBold52: Font = .custom("SpoqaHanSansNeo-Bold", size: 52)
+    static let spoqaBold52 = GSFontStyle(font: .custom("SpoqaHanSansNeo-Bold", size: 52), size: 52)
 }
