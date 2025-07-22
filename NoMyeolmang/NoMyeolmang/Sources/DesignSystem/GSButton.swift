d6cb8d47e8225dab74fbc22701a7175a4157dcdf
wish627 <wishful.to@gmail.com>
2025-07-22 16:22:02 +0900

    feat: #44 버튼 투명도 변경
    새 브랜치에서 버튼 커스텀화 진행 예정

--- NoMyeolmang/NoMyeolmang/Sources/DesignSystem/GSButton.swift
+++ NoMyeolmang/NoMyeolmang/Sources/DesignSystem/GSButton.swift
@@ -22,11 +22,20 @@ struct GSButton: View {
         .frame(width: width, height: 44)
         .background(
             RoundedRectangle(cornerRadius: 10)
-                .fill(Color.white.opacity(0.2))
+                .fill(Color.white.opacity(0.14))
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(Color.white.opacity(0.7), lineWidth: 1)
                 )
+            
+//            ZStack {
+//                BlurView()
+//                    .clipShape(RoundedRectangle(cornerRadius: 10))
+//                RoundedRectangle(cornerRadius: 10)
+//                    .fill(Color.white.opacity(0.00000000001))
+//                RoundedRectangle(cornerRadius: 10)
+//                    .stroke(Color.white.opacity(0.7), lineWidth: 1)
+//            }
         )
         .shadow(color: Color.black.opacity(0.0168), radius: 2.21, x: 0, y: 2.77)
         .shadow(color: Color.black.opacity(0.022), radius: 5.32, x: 0, y: 6.65)
