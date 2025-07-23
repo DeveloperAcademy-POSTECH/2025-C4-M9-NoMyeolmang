8ba7ab0a3a82c01d8f8e2005c8bf8ab5d04c129e
MuchanKim <mutopia82@gmail.com>
2025-07-23 22:16:26 +0900

    feat: #55 Features 데이터 모델 정의
    - 모델 input에 들어가는 6개의 피처입니다.

---
+++ NoMyeolmang/NoMyeolmang/Sources/ML/Models/Features.swift
@@ -0,0 +1,15 @@
+//
+//  Featrues.swift
+//  NoMyeolmang
+//
+//  Created by Muchan Kim on 7/23/25.
+//
+
+struct Features: Codable {
+    let blinkCountPerMin: Double
+    let faceBodyPresent: Double
+    let phonePresent: Double
+    let elapsedTime: Double
+    let yawnPerMin: Double
+    let longBlinkPerMin: Double
+}
