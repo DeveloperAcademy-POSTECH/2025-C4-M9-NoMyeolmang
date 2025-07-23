81cf9bec86a2b19779c669ff74ce2bf5174f1cf5
MuchanKim <mutopia82@gmail.com>
2025-07-23 22:22:43 +0900

    feat: #55 MinMaxScaler 업데이트
    - 하품, 긴 눈깜빡임 추가 + 클리핑 처리를 하도록 구현하였습니다.

--- NoMyeolmang/NoMyeolmang/Sources/ML/Utilities/MinMaxScaler.swift
+++ NoMyeolmang/NoMyeolmang/Sources/ML/Utilities/MinMaxScaler.swift
@@ -13,28 +13,56 @@ enum MinMaxScaler {
             max: Constants.blinkCounterPerMaxValue
         )
     }
-    
+
     static func scaleFace(_ value: Double) -> Double {
-        return scale(value: value, min: Constants.faceBodyPresentMinValue, max: Constants.faceBodyPresentMaxValue)
+        return scale(
+            value: value,
+            min: Constants.faceBodyPresentMinValue,
+            max: Constants.faceBodyPresentMaxValue
+        )
     }
-    
+
     static func scalePhone(_ value: Double) -> Double {
-        return scale(value: value, min: Constants.phonePresentMinValue, max: Constants.phonePresentMaxValue)
+        return scale(
+            value: value,
+            min: Constants.phonePresentMinValue,
+            max: Constants.phonePresentMaxValue
+        )
     }
-    
+
     static func scaleTime(_ value: Double) -> Double {
-        return scale(value: value, min: Constants.elapsedTimeMinValue, max: Constants.elapsedTimeMaxValue)
+        return scale(
+            value: value,
+            min: Constants.elapsedTimeMinValue,
+            max: Constants.elapsedTimeMaxValue
+        )
     }
-    
+
+    static func scaleYawn(_ value: Double) -> Double {
+        return safeScale(
+            value: value,
+            min: Constants.yawnCountPerMinValue,
+            max: Constants.yawnCounterPerMaxValue
+        )
+    }
+
+    static func scaleLongBlink(_ value: Double) -> Double {
+        return safeScale(
+            value: value,
+            min: Constants.longBlinkCountPerMinValue,
+            max: Constants.longBlinkCounterPerMaxValue
+        )
+    }
+
     private static func scale(value: Double, min: Double, max: Double) -> Double {
         guard max != min else { return 0.0 }
         return (value - min) / (max - min)
     }
-    
+
     private static func softClip(value: Double, min: Double, max: Double) -> Double {
         return Swift.max(min, Swift.min(value, max))
     }
-    
+
     private static func safeScale(value: Double, min: Double, max: Double) -> Double {
         let clippedValue = softClip(value: value, min: min, max: max)
         return scale(value: clippedValue, min: min, max: max)
