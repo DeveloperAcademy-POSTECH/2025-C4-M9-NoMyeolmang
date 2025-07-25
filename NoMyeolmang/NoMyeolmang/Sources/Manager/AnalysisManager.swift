f07101e6cd93026e8061ea4fb12cdca5bb677a44
Moo <mutopia82@gmail.com>
2025-07-25 11:29:47 +0900

    feat: #74 AnalysisManager 리턴 값 조정 및 일부 코드 개선
    - return 값을 Features로 변경하였습니다.

--- NoMyeolmang/NoMyeolmang/Sources/Manager/AnalysisManager.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Manager/AnalysisManager.swift
@@ -2,10 +2,9 @@
 //  AnalysisManager.swift
 //  NoMyeolmang
 //
-//  Created by Moo on 7/21/25.
+//  Updated by Moo on 7/24/25.
 //
 
-import AVFoundation
 import Vision
 
 final class AnalysisManager {
@@ -16,8 +15,13 @@ final class AnalysisManager {
     private(set) var lastBlinkCount: Int = 0
     private(set) var lastYawnCount: Int = 0
     private(set) var lastAbsenceTime: TimeInterval = 0
+    private(set) var elapsedTime: Int = 0
 
-    init(blinkDetector: BlinkDetector, yawnDetector: YawnDetector, presenceTimer: FaceAbsenceTimer) {
+    init(
+        blinkDetector: BlinkDetector,
+        yawnDetector: YawnDetector,
+        presenceTimer: FaceAbsenceTimer
+    ) {
         self.blinkDetector = blinkDetector
         self.yawnDetector = yawnDetector
         self.presenceTimer = presenceTimer
@@ -32,7 +36,7 @@ final class AnalysisManager {
     }
 
     func analyze(pixelBuffer: CVPixelBuffer) async {
-        let faceObservation = try? await performFaceLandmarksDetection(on: pixelBuffer)
+        let faceObservation = await performFaceLandmarksDetection(on: pixelBuffer)
         let landmarks = faceObservation?.landmarks
 
         let isFacePresent = (faceObservation != nil)
@@ -42,9 +46,20 @@ final class AnalysisManager {
         lastYawnCount = yawnDetector.detect(from: landmarks)
         lastAbsenceTime = presenceTimer.totalAbsenceTime
     }
-    
-    func reset() {
-        print("1분간 눈깜빡임: \(lastBlinkCount), 하품: \(lastYawnCount), 자리비움(초): \(Int(lastAbsenceTime))")
+
+    func generateFeatures() -> Features {
+        elapsedTime += 1
+        return Features(
+            blinkCountPerMin: Double(lastBlinkCount),
+            faceBodyPresent: Double(lastAbsenceTime),
+            phonePresent: 0.1,
+            elapsedTime: Double(elapsedTime),
+            yawnPerMin: Double(lastYawnCount),
+            longBlinkPerMin: 1.0
+        )
+    }
+
+    func resetState() {
         blinkDetector.reset()
         yawnDetector.reset()
         presenceTimer.reset()
@@ -53,24 +68,24 @@ final class AnalysisManager {
         lastAbsenceTime = 0
     }
 
-    private func performFaceLandmarksDetection(on pixelBuffer: CVPixelBuffer) async throws -> VNFaceObservation {
-        try await withCheckedThrowingContinuation { continuation in
+    func readyForSession() {
+        elapsedTime = 0
+        resetState()
+    }
+
+    private func performFaceLandmarksDetection(on pixelBuffer: CVPixelBuffer) async -> VNFaceObservation? {
+        await withCheckedContinuation { continuation in
             let request = VNDetectFaceLandmarksRequest()
-            let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
+            let handler = VNImageRequestHandler(
+                cvPixelBuffer: pixelBuffer,
+                orientation: .up,
+                options: [:]
+            )
             do {
                 try handler.perform([request])
-                if let faces = request.results, let face = faces.first {
-                    continuation.resume(returning: face)
-                } else {
-                    continuation.resume(
-                        throwing: NSError(domain: "AnalysisManager",
-                                          code: 1,
-                                          userInfo: [NSLocalizedDescriptionKey: "No face detected"]
-                                         )
-                    )
-                }
+                continuation.resume(returning: request.results?.first)
             } catch {
-                continuation.resume(throwing: error)
+                continuation.resume(returning: nil)
             }
         }
     }
