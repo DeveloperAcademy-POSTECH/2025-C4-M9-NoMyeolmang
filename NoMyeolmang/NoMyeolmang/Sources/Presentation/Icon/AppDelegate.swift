0c8d886ba4e7171b53fb8dec7ce56965bd5b9f3f
wish627 <wishful.to@gmail.com>
2025-07-25 18:02:50 +0900

    feat: #75 상단바 타이머

--- NoMyeolmang/NoMyeolmang/Sources/Presentation/Icon/AppDelegate.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/Icon/AppDelegate.swift
@@ -11,13 +11,19 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem?
     var popover = NSPopover()
+    var countdownTimer: Timer?
+    var remainingSeconds: Int = 30 * 60
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        
         popover.contentSize = NSSize(width: 230, height: 270)
         popover.behavior = .transient
         popover.contentViewController = NSViewController()
-        popover.contentViewController?.view = NSHostingView(rootView: PopoverView())
+        popover.contentViewController?.view = NSHostingView(
+            rootView: PopoverView(onStart: {
+                self.popover.performClose(nil)
+                self.startCountdown()
+            })
+        )
 
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
@@ -37,5 +43,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             popover.contentSize = NSSize(width: 230, height: 270)
             popover.show(relativeTo: .zero, of: button, preferredEdge: .minY)
         }
+    }
+    func startCountdown() {
+        updateStatusItemTitle()
+
+        countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            self.remainingSeconds -= 1
+            self.updateStatusItemTitle()
+
+            if self.remainingSeconds <= 0 {
+                self.countdownTimer?.invalidate()
+                self.statusItem?.button?.title = "Done" // 타이머 끝나면 어떻게 보여줄건지 확인하기
+            }
+        }
+    }
+
+    func updateStatusItemTitle() {
+        let minutes = remainingSeconds / 60
+        let seconds = remainingSeconds % 60
+        statusItem?.button?.title = String(format: "%02d:%02d", minutes, seconds)
     }
 }
--- NoMyeolmang/NoMyeolmang/Sources/Presentation/Icon/PopoverView.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/Icon/PopoverView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct PopoverView: View {
+    var onStart: () -> Void
 
     var body: some View {
         VStack(spacing: 20) {
             Text("🌠🎸")
                 .textStyle(GSFont.SemiBold18)
             Button("시작하기") {
+                onStart() // 타이머뷰에도 담아야함 (다른 브랜치에서 작업 예정)
             }
         }
         .frame(width: 230, height: 270)
