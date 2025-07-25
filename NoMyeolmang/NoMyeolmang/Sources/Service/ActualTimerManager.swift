075b3410a2240163dfa1816fc67374818e5d736a
ohdodin <ohdodin@ohdodinui-MacBookPro.local>
2025-07-26 01:20:19 +0900

    Fix: #76 actualTimer에 종료 콜백함수 추가

--- NoMyeolmang/NoMyeolmang/Sources/Service/ActualTimerManager.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Service/ActualTimerManager.swift
@@ -16,11 +16,13 @@ final class ActualTimerManager {
     private var timer: Timer?
     private var interval: TimeInterval = 1.0
     private var isRepeating: Bool = true
-    private var count: Int = 0
     private var goalTime: Int = 300
+    var count: Int = 0
 
     // 타이머 이벤트 알림
     var onTick: ((Int) -> Void)?
+
+    var onFinish: (() -> Void)?
 
     // MARK: Public Methods
     func start(interval: TimeInterval = 1.0, repeats: Bool = true) {
@@ -29,6 +31,9 @@ final class ActualTimerManager {
         self.interval = interval
         self.isRepeating = repeats
         self.count = 0
+        
+        // 🔹 타이머 시작 직전에 즉시 1회 호출
+        self.onTick?(self.count)
 
         timer = Timer.scheduledTimer(
             withTimeInterval: interval,
@@ -37,6 +42,11 @@ final class ActualTimerManager {
                 guard let self else { return }
                 self.count += 1
                 self.onTick?(self.count)
+
+                if self.count >= self.goalTime {
+                    self.onFinish?()
+                    self.stop()
+                }
             }
         )
     }
@@ -44,7 +54,7 @@ final class ActualTimerManager {
     func stop() {
         clear()
     }
-    
+
     func setGoalTime(newGoalTime: Int) {
         self.goalTime = newGoalTime
         print(self.goalTime)
@@ -53,11 +63,11 @@ final class ActualTimerManager {
     var isRunning: Bool {
         return timer?.isValid ?? false
     }
-    
+
     var lastingTime: Int {
         return goalTime - count
     }
-    
+
     // MARK: Private Methods
     private func clear() {
         timer?.invalidate()
