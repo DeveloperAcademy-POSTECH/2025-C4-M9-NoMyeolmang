0e15222e4f9024331190f131a205d68417146996
wish627 <wishful.to@gmail.com>
2025-07-26 10:42:07 +0900

    feat : #62 OnboardingView 분리

---
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/Onboarding/OnboardingView.swift
@@ -0,0 +1,77 @@
+//
+//  OnboardingView.swift
+//  NoMyeolmang
+//
+//  Created by 김소원 on 7/26/25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @Binding var showOnboarding: Bool
+    @State private var popupStep: Int = 0
+    let popupData: [(String, String)] = [
+        ("끊기지 않는 학습 리듬을 위해", "눈 깜빡임, 하품 같은 몸의 반응을 분석해\n몰입 상태를 실시간으로 알려드려요.\n집중력이 떨어지는 순간, 다시 몰입을 도와드릴게요."),
+        ("영상은 저장되지 않아요", "얼굴을 촬영하거나 영상을 저장하지 않아요.\n모든 분석은 기기 내에서만 이루어지며.\n오직 집중을 돕기 위해서 사용해요."),
+        ("나만의 집중 리듬을 찾아요", "공부가 끝난 뒤 집중도를 체크하는\n간단한 확인을 거쳐\n당신에게 딱 맞는 몰입 시간을 추천해드려요.")
+    ]
+
+    var body: some View {
+        ZStack {
+            Image("SpaceshipBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            Color("252525")
+                .opacity(0.55)
+                .edgesIgnoringSafeArea(.all)
+
+                VStack(spacing: 0) {
+                    Image("togglemask")
+                        .frame(width: 235, height: 40)
+                    
+                    FocusTimeSetting()
+                        .frame(width: 329, height: 161)
+                        .padding(.top, 24)
+                    
+                    ZStack {
+                        Image("buttonmask")
+                            .frame(width: 250, height: 44)
+                    }
+                    .padding(.top, 39)
+                }
+                .padding(.top, 95)
+                .padding(.bottom, 200)
+
+            if popupStep < popupData.count {
+                PopupGuideView(
+                    message: popupData[popupStep].0,
+                    notice: popupData[popupStep].1,
+                    onNext: { popupStep += 1 }
+                )
+            } else if popupStep == popupData.count {
+                GuidanceTooltipViewStep1(
+                    total: 2,
+                    onConfirm: { popupStep += 1 }
+                )
+                .offset(x: -265, y: 80)
+                .zIndex(1)
+            } else if popupStep == popupData.count + 1 {
+                GuidanceTooltipViewStep2(
+                    total: 2,
+                    onConfirm: {
+                        showOnboarding = false
+                    }
+                )
+                .offset(x: -225, y: -60)
+                .zIndex(1)
+            }
+        }
+    }
+}
+
+#Preview {
+    OnboardingView(showOnboarding: .constant(true))
+        .frame(width: 800, height: 600)
+}
--- NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerSettingView/TimerSettingView.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerSettingView/TimerSettingView.swift
@@ -10,12 +10,7 @@ import SwiftUI
 struct TimerSettingView: View {
     @EnvironmentObject var coordinator: AppCoordinator
     @State private var isRecommendedSelected = true
-    @State private var popupStep: Int = 0
-    let popupData: [(String, String)] = [
-        ("끊기지 않는 학습 리듬을 위해", "눈 깜빡임, 하품 같은 몸의 반응을 분석해\n몰입 상태를 실시간으로 알려드려요.\n집중력이 떨어지는 순간, 다시 몰입을 도와드릴게요."),
-        ("영상은 저장되지 않아요", "얼굴을 촬영하거나 영상을 저장하지 않아요.\n모든 분석은 기기 내에서만 이루어지며.\n오직 집중을 돕기 위해서 사용해요."),
-        ("나만의 집중 리듬을 찾아요", "공부가 끝난 뒤 집중도를 체크하는\n간단한 확인을 거쳐\n당신에게 딱 맞는 몰입 시간을 추천해드려요.")
-    ]
+    @State private var showOnboarding = true
     
     var body: some View {
         
@@ -28,53 +23,29 @@ struct TimerSettingView: View {
                 .resizable()
                 .ignoresSafeArea()
                 .scaledToFill()
-                .frame(width: 800, height: 600, alignment: .center)
+              //  .frame(width: 800, height: 600, alignment: .center)
             
-            VStack(spacing: 0) {
-                ToggleTabView(isRecommendedSelected: $isRecommendedSelected)
-                
-                ZStack(alignment: .top) {
+            if showOnboarding {
+                OnboardingView(showOnboarding: $showOnboarding)
+            }
+            
+            if !showOnboarding {
+                VStack(spacing: 0) {
+                    ToggleTabView(isRecommendedSelected: $isRecommendedSelected)
+                    
                     FocusTimeSetting()
                         .frame(width: 329)
                         .padding(.top, 24)
                     
-                    if popupStep == popupData.count {
-                        GuidanceTooltipViewStep1(
-                            total: 2,
-                            onConfirm: { popupStep += 1 }
-                        )
-                        .offset(x: -265, y: 80)
-                    }
-                }
-                
-                ZStack {
-                    GSButton(title: "집중 시작하기", width: 250) {
-                        coordinator.push(.timer)
-                    }
-                }
-                .overlay(
-                    Group {
-                        if popupStep == popupData.count + 1 {
-                            GuidanceTooltipViewStep2(
-                                total: 2,
-                                onConfirm: { popupStep += 1 }
-                            )
-                            .offset(x: -225, y: -60)
+                    ZStack {
+                        GSButton(title: "집중 시작하기", width: 250) {
+                            coordinator.push(.timer)
                         }
-                    },
-                    alignment: .top
-                )
-                .padding(.top, 39)
-            }
-            .padding(.top, 95)
-            .padding(.bottom, 200)
-            
-            if popupStep < popupData.count {
-                PopupGuideView(
-                    message: popupData[popupStep].0,
-                    notice: popupData[popupStep].1,
-                    onNext: { popupStep += 1 }
-                )
+                    }
+                    .padding(.top, 39)
+                }
+                .padding(.top, 95)
+                .padding(.bottom, 200)
             }
         }
         .frame(minWidth: 800, minHeight: 600)
