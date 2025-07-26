031151d489663fbc40f38d358053a7b65d552b7a
오서진 <seojin2414@gmail.com>
2025-07-26 13:31:46 +0900

    Fix: #76 actualTimer에 종료 콜백함수 추가 (#88)
    * Fix: #76 actualTimer에 종료 콜백함수 추가
    
    * Fix: #76 count 변수 reset 타이밍 변경
    
    ---------
    
    Co-authored-by: ohdodin <ohdodin@ohdodinui-MacBookPro.local>

--- NoMyeolmang/NoMyeolmang/Sources/Service/ActualTimerManager.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Service/ActualTimerManager.swift
@@ -16,11 +16,13 @@ final class ActualTimerManager {
     private var timer: Timer?
     private var interval: TimeInterval = 1.0
     private var isRepeating: Bool = true
-    private var count: Int = 0
     private var goalTime: Int = 300
+    var count: Int = 0
 
     // 타이머 이벤트 알림
     var onTick: ((Int) -> Void)?
+
+    var onFinish: (() -> Void)?
 
     // MARK: Public Methods
     func start(interval: TimeInterval = 1.0, repeats: Bool = true) {
@@ -28,7 +30,9 @@ final class ActualTimerManager {
 
         self.interval = interval
         self.isRepeating = repeats
-        self.count = 0
+        
+        // 🔹 타이머 시작 직전에 즉시 1회 호출
+        self.onTick?(self.count)
 
         timer = Timer.scheduledTimer(
             withTimeInterval: interval,
@@ -37,6 +41,11 @@ final class ActualTimerManager {
                 guard let self else { return }
                 self.count += 1
                 self.onTick?(self.count)
+
+                if self.count >= self.goalTime {
+                    self.onFinish?()
+                    self.stop()
+                }
             }
         )
     }
@@ -44,7 +53,7 @@ final class ActualTimerManager {
     func stop() {
         clear()
     }
-    
+
     func setGoalTime(newGoalTime: Int) {
         self.goalTime = newGoalTime
         print(self.goalTime)
@@ -53,13 +62,14 @@ final class ActualTimerManager {
     var isRunning: Bool {
         return timer?.isValid ?? false
     }
-    
+
     var lastingTime: Int {
         return goalTime - count
     }
-    
+
     // MARK: Private Methods
     private func clear() {
+        self.count = 0
         timer?.invalidate()
         timer = nil
     }
