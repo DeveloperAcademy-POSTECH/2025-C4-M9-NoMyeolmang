059a0e4be1313d2c98e52e195c1f624c70b0204c
Moo <mutopia82@gmail.com>
2025-07-28 00:23:38 +0900

    feat: #67 기존 뷰 뷰모델 연결
    - 기존 뷰의 변경을 최소화 했습니다.

---
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerSettingView/View/TimerSettingView.swift
@@ -0,0 +1,56 @@
+//
+//  TimerSettingView.swift
+//  NoMyeolmang
+//
+//  Created by 김소원 on 7/20/25.
+//
+
+import SwiftUI
+
+struct TimerSettingView: View {
+    @EnvironmentObject var coordinator: AppCoordinator
+    @ObservedObject var viewModel: TimerSettingViewModel
+    
+    var backgroundView: some View {
+        ZStack {
+            Image("SpaceshipBackground")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .allowsHitTesting(false)
+        }
+    }
+    
+    var body: some View {
+        
+        ZStack {
+            backgroundView
+            
+            VStack(spacing: 0) {
+                ToggleTabView(isRecommendedSelected: $viewModel.selectedTab, goalTime: $viewModel.goalTime)
+                
+                Group {
+                    if viewModel.selectedTab == .recommended {
+                        FocusTimeSetting(goalTime: $viewModel.goalTime)
+                    } else {
+                        PersonalTimerSettingView(goalTime: $viewModel.goalTime)
+                    }
+                }
+                .frame(width: 329)
+                .padding(.top, 24)
+                
+                ZStack {
+                    GSButton(title: "집중 시작하기", width: 250) {
+                        viewModel.startFocusSession()
+                        coordinator.push(.timer)
+                    }
+                }
+                .padding(.top, 39)
+            }
+            .padding(.top, 95)
+            .padding(.bottom, 200)
+        }
+        .frame(minWidth: 800, minHeight: 600)
+        .navigationBarBackButtonHidden(true)
+    }
+}
---
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerSettingView/View/ToggleTabView.swift
@@ -0,0 +1,76 @@
+//
+//  ToggleTabView.swift
+//  NoMyeolmang
+//
+//  Created by 김소원 on 7/23/25.
+//
+
+import SwiftUI
+
+struct ToggleTabView: View {
+    @Binding var isRecommendedSelected: TabType
+    @Binding var goalTime: Int
+
+    var body: some View {
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 40)
+                .fill(Color.white.opacity(0.04))
+                .stroke(Color.white.opacity(0.55))
+                .frame(width: 235, height: 40)
+
+            RoundedRectangle(cornerRadius: 40)
+                .fill(Color("#9D86DB80").opacity(0.55))
+                .frame(width: 110, height: 32)
+                .offset(x: isRecommendedSelected == .recommended ? 5 : 120)
+                .shadow(color: Color.black.opacity(0.25), radius: 7, x: 0, y: 0)
+                .allowsHitTesting(false)
+                .animation(.spring(response: 0.3, dampingFraction: 0.7), value: isRecommendedSelected)
+
+            HStack {
+                Spacer().frame(width: 34.5)
+
+                Text("맞춤 추천")
+                    .textStyle(GSFont.SemiBold14)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .onTapGesture {
+                        withAnimation {
+                            isRecommendedSelected = .recommended
+                            goalTime = 30  // 추천 시간으로 설정
+                        }
+                    }
+
+                Spacer().frame(width: 64)
+
+                Text("직접 설정")
+                    .textStyle(GSFont.SemiBold14)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .onTapGesture {
+                        withAnimation {
+                            isRecommendedSelected = .personal
+                        }
+                    }
+
+                Spacer().frame(width: 34.5)
+            }
+            .frame(width: 235, height: 40)
+        }
+        .frame(width: 235, height: 40)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    struct ToggleTabPreviewWrapper: View {
+        @State private var selectedTab: TabType = .recommended
+        @State private var goalTime = 30
+
+        var body: some View {
+            ToggleTabView(isRecommendedSelected: $selectedTab, goalTime: $goalTime)
+                .padding()
+                .background(Color.black)
+        }
+    }
+
+    return ToggleTabPreviewWrapper()
+}
