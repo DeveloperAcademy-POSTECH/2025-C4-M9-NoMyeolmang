7f8e60311d514afa7096814bfad65b5b0207c51e
Moo <mutopia82@gmail.com>
2025-07-28 23:52:00 +0900

    chore: #124 단순 파일 위치 변경

---
+++ NoMyeolmang/NoMyeolmang/Sources/App/Coordinator/AppCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  AppCoordinator.swift
+//  NoMyeolmang
+//
+//  Created by 김소원 on 7/20/25.
+//
+
+import SwiftUI
+
+@MainActor
+final class AppCoordinator: ObservableObject {
+    @Published var path = NavigationPath()
+
+    func push(_ route: AppRoute) {
+        path.append(route)
+    }
+
+    func popToRoot() {
+        path.removeLast(path.count)
+    }
+
+    func replaceLast(with route: AppRoute) {
+        if !path.isEmpty {
+            path.removeLast()
+        }
+        path.append(route)
+    }
+}
