47c896a2510d0a5ff9cfe6e7a1386ddd282a8d11
ohdodin <ohdodin@ohdodinui-MacBookPro.local>
2025-07-29 19:01:24 +0900

    Fix: #131 텍스트 입력을 받을 수 있도록 styleMask 수정

--- NoMyeolmang/NoMyeolmang/Sources/Presentation/Popover/PopoverWindowController.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/Popover/PopoverWindowController.swift
@@ -11,16 +11,18 @@ import SwiftUI
 class PopoverWindowController: NSWindowController {
     init(rootView: some View) {
         let hostingView = NSHostingView(rootView: rootView)
-        let window = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 230, height: 270),
-                              styleMask: [.borderless],
-                              backing: .buffered,
-                              defer: false)
+        let window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 230, height: 270),
+            styleMask: [.titled, .closable],
+            backing: .buffered,
+            defer: false
+        )
         window.contentView = hostingView
         window.isOpaque = false
         window.backgroundColor = .clear
         window.hasShadow = true
         window.level = .floating
-        
+
         super.init(window: window)
     }
 
