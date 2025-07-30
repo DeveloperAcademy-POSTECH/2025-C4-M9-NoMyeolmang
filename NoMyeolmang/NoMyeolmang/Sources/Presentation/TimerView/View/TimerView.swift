90ae1c4bc6f2d3e06e8fb1d605ccb01cafc8ef0a
ohdodin <ohdodin@ohdodinui-MacBookPro.local>
2025-07-31 02:21:59 +0900

    Fix: #152 버튼 숨겨!

--- NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerView/View/TimerView.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerView/View/TimerView.swift
@@ -10,34 +10,45 @@ import SwiftUI
 struct TimerView: View {
     @EnvironmentObject var coordinator: AppCoordinator
     @ObservedObject var viewModel: TimerViewModel
-    @State private var isSpaceshipOut: Bool = false // 우주선 애니메이션 상태 관리
-    @State private var isTimerOut: Bool = false // 텍스트 + 타이머 + 버튼 애니메이션 상태 관리
+    @State private var isSpaceshipOut: Bool = false  // 우주선 애니메이션 상태 관리
+    @State private var isTimerOut: Bool = false  // 텍스트 + 타이머 + 버튼 애니메이션 상태 관리
 
     var body: some View {
         GeometryReader { geo in
             ZStack(alignment: .center) {
                 TimerBackgroundView(
-                    animationDuration: viewModel.backgroundDuration, sessionState: $viewModel.sessionState
+                    animationDuration: viewModel.backgroundDuration,
+                    sessionState: $viewModel.sessionState
                 )
 
                 SpaceshipView()
                     .frame(maxWidth: .infinity, alignment: .center)
-                    .offset(y: isSpaceshipOut ? -geo.size.height : 0) // isSpaceshipOut일 때 우주선 위로 사라짐
+                    .offset(y: isSpaceshipOut ? -geo.size.height : 0)  // isSpaceshipOut일 때 우주선 위로 사라짐
 
                 VStack(alignment: .center, spacing: 32) {
+                    Spacer()
                     TimeLeftView(remainingTimeText: viewModel.formattedTime)
 
                     TimerStopButton {
                         viewModel.stopSession()
                         coordinator.pop()
                     }
-                    Button("Next: Feedback") {
+                    Spacer()
+                    Button {
                         viewModel.handleTimerFinished()
+                    } label: {
+                        Text(">")
+                            .foregroundColor(.white)
+                            .opacity(0.4)
+                            .font(.title3)
                     }
                     .navigationBarBackButtonHidden(true)
+                    .buttonStyle(.plain)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding()
                 }
                 .frame(maxWidth: .infinity, alignment: .center)
-                .opacity(isTimerOut ? 0 : 1) // isTimerOut일 때 타이머 + 버튼 fade out
+                .opacity(isTimerOut ? 0 : 1)  // isTimerOut일 때 타이머 + 버튼 fade out
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
@@ -56,12 +67,12 @@ struct TimerView: View {
     private func handleSessionStateChange(_ new: TimerViewState?) {
         guard let new else { return }
         if new == .isCompleted {
-            withAnimation(.easeInOut(duration: 0.5)) { // 타이머 종료시 fade out 적용 효과
+            withAnimation(.easeInOut(duration: 0.5)) {  // 타이머 종료시 fade out 적용 효과
                 isTimerOut = true
-            } completion: { // 타이머 사라지고 우주선 애니메이션 시작
+            } completion: {  // 타이머 사라지고 우주선 애니메이션 시작
                 withAnimation(.easeInOut(duration: 1)) {
                     isSpaceshipOut = true
-                } completion: { // 우주선 애니메이션 끝나면 피드백 뷰로 이동
+                } completion: {  // 우주선 애니메이션 끝나면 피드백 뷰로 이동
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                         coordinator.push(.feedback)
                     }
