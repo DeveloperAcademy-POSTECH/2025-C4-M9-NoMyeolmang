5755b29bd70033d3e1fb4370681faeaa486dda57
Yujin Jeon <166476876+romiwaves@users.noreply.github.com>
2025-07-30 22:39:30 +0900

    fix: #140 타이머뷰 텍스트와 타이머 사이 간격 조절, 타이머 format 조정, 자간 조절

--- NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerView/View/TimeLeftView.swift
+++ NoMyeolmang/NoMyeolmang/Sources/Presentation/TimerView/View/TimeLeftView.swift
@@ -15,11 +15,11 @@ struct TimeLeftView: View {
             Text("남은 시간")
                 .textStyle(GSFont.Regular16)
                 .foregroundStyle(Color.white)
-                .padding(.bottom, 0.1)
+
             Text(remainingTimeText)
                 .foregroundStyle(Color.white)
                 .font(.custom("SpoqaHanSansNeo-Bold", size: 52))
-                .tracking(1)
+                .tracking(-2)
         }
     }
 }
